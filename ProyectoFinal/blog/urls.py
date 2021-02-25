c17fc4a68f98260fd7eb6d7a9f7b952fce0d3278
Fernando_Vazquez <61173776+Chilangdon20@users.noreply.github.com>
2021-02-25 10:58:49 -0600

    Actualizacion de url y categorias de la seccion blog

--- ProyectoFinal/blog/urls.py
+++ ProyectoFinal/blog/urls.py
@@ -3,9 +3,10 @@ from . import views
 
 
 # lista que contiene las URL´s creadas , las cuales se conectan con las vistas
+# prueba
 urlpatterns = [
 
     path('',views.blog,name="Blog"),
-    path('categoria/<categoria_id>'),
+    path('categoria/<int:categoria_id>',views.categoria, name="categoria"),
 
 ]
--- ProyectoFinal/blog/views.py
+++ ProyectoFinal/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from blog.models import Post
+from blog.models import Post,Categoria
 
 # Create your views here.
 # <img class="intro-img img-fluid mb-3 mb-lg-0 rounded" src="{{post.imagen.url}}" alt="" style="width: 50%;">
@@ -12,3 +12,15 @@ def blog(request):
     """
     posts=Post.objects.all()
     return render(request, 'blog/blog.html', {"posts": posts})
+
+def categoria(request,categoria_id):
+    """
+
+    :param request:
+    :param categoria_id:
+    :return: listado de post correspondientes a la categoria
+    seleccionada
+    """
+    categoria=Categoria.objects.get(id=categoria_id)
+    posts = Post.objects.filter(categorias=categoria)
+    return render(request,'blog/categorias.html', {'categoria':categoria,"posts": posts})
