2b56cb57b6a57697309d26f24128e0d50d11ff9b
Fernando_Vazquez <61173776+Chilangdon20@users.noreply.github.com>
2021-03-02 10:48:48 -0600

    ACTUALIZACION

--- ProyectoFinal/contacto/views.py
+++ ProyectoFinal/contacto/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import  FormularioContacto
 # Create your views here.
 def contacto(request):
@@ -8,5 +8,20 @@ def contacto(request):
     :return Solicitud render de URL ,acceso a la vista contacto :
 
     """
-    formulario_contacto  = FormularioContacto()
+    formulario_contacto=FormularioContacto()
+
+    """
+    rescatar y validar la informacion del usuario , en caso
+    de que todos los campos esten llenos.
+    
+    
+    """
+    if request.method=="POST":
+        formulario_contacto=FormularioContacto(data=request.POST)
+        if formulario_contacto.is_valid():
+            nombre=request.POST.get("nombre")
+            email = request.POST.get("email")
+            contenido = request.POST.get("contenido")
+
+            return redirect("/contacto/?valido")
     return render(request, 'contacto/contacto.html',{'miFormulario':formulario_contacto})
