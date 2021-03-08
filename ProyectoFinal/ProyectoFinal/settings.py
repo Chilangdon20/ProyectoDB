d15bf52d0fc878d74ccef05600c6f1e1a33148ae
Fernando_Vazquez <61173776+Chilangdon20@users.noreply.github.com>
2021-03-08 10:28:41 -0600

    Update settings.py

--- ProyectoFinal/ProyectoFinal/settings.py
+++ ProyectoFinal/ProyectoFinal/settings.py
@@ -132,5 +132,5 @@ EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST="smtp.gmail.com"
 EMAIL_USE_TLS=True
 EMAIL_PORT=587
-EMAIL_HOST_USER="chilangdonvazquez@gmail.com"
-EMAIL_HOST_PASSWORD='56758062'
+EMAIL_HOST_USER=""
+EMAIL_HOST_PASSWORD=''
